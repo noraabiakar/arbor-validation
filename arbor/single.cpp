e99c9aef43f07e669f69f2e82d591a9790a5bf17
abiakarn <nora.abiakar@gmail.com>
2019-05-27 17:06:13 +0200

    soma with expsyn and pas + dendrite - perfect match

--- arbor/single.cpp
+++ arbor/single.cpp
@@ -250,8 +250,8 @@ arb::cable_cell single_cell(const single_params& params) {
     // Add soma.
     auto soma = cell.add_soma(11.65968/2.0);
 
-    /*auto dend = cell.add_cable(0, arb::section_kind::dendrite, 3.0/2.0, 3.0/2.0, 200);
-    dend->set_compartments(2000);*/
+    auto dend = cell.add_cable(0, arb::section_kind::dendrite, 3.0/2.0, 3.0/2.0, 200);
+    dend->set_compartments(2000);
 
     if (params.hh) {
         auto hh = arb::mechanism_desc("hh");
